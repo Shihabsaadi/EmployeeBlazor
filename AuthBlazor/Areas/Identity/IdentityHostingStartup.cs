5714bc90be62648004dca72df13cb6986d1f03c0
Shihab Saadi <s.saadi2047@gmail.com>
2021-12-07 14:33:24 +0600

    Data Grid and auth

---
+++ AuthBlazor/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AuthBlazor.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AuthBlazor.Areas.Identity.IdentityHostingStartup))]
+namespace AuthBlazor.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
