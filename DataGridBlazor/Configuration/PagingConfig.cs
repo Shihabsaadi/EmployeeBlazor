974132bb1aa5cd54edde675ed863d9989ea6a57d
Shihab Saadi <s.saadi2047@gmail.com>
2021-12-11 13:39:00 +0600

    Paging

---
+++ DataGridBlazor/Configuration/PagingConfig.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace DataGridBlazor.Configuration
+{
+    public class PagingConfig
+    {
+        public bool Enabled { get; set; }
+        public int PageSize {get;set;}
+        public int PageCount {get;set;}
+        public bool CustomPager { get; set; }
+        public int NumOfItemsToSkip(int pageNumber)
+        {
+            if(Enabled)
+            {
+                return (pageNumber-1)*PageSize;
+            }
+            return 0;
+        }
+        public int NumOfItemsToTake(int totalItemsCount)
+        {
+            if (Enabled)
+            {
+                return PageSize;
+            }
+            return totalItemsCount;
+        }
+        public int PrevPageNumber(int currentPageNumber)
+        {
+            if(currentPageNumber>1)
+                return currentPageNumber-1;
+            else
+                return 1;
+        }
+        public int NextPageNumber(int currentPageNumber,int totalItemsCount)
+        {
+            if (currentPageNumber < MaxPageNumber(totalItemsCount))
+                return currentPageNumber + 1;
+            else
+                return 1;
+        }
+        public int MaxPageNumber(int totalItemsCount)
+        {
+            int maxPageNumber = 0;
+            double numberOfPages = (double)totalItemsCount / (double)PageSize;
+            if (numberOfPages == Math.Floor(numberOfPages))
+                maxPageNumber = (int)numberOfPages;
+            else
+                maxPageNumber=(int)numberOfPages+1;
+            return maxPageNumber;
+
+        }
+
+    }
+}
